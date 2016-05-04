2c22350616a8766fc6545c976c50c7117709df41
photos <myjellymail@gmail.com>
2016-05-04 15:32:45 -0700

    Part 20 Completed

---
+++ MusicVideo/SettingsTVC.swift
@@ -0,0 +1,45 @@
+//
+//  SettingsTVC.swift
+//  MusicVideo
+//
+//  Created by Forrest Collins on 5/4/16.
+//  Copyright © 2016 bananapps. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTVC: UITableViewController {
+
+    @IBOutlet weak var aboutDisplay: UILabel!
+    @IBOutlet weak var feedbackDisplay: UILabel!
+    @IBOutlet weak var securityDisplay: UILabel!
+    
+    @IBOutlet weak var touchID: UISwitch!
+    @IBOutlet weak var bestImageDisplay: UILabel!
+    @IBOutlet weak var apiCount: UILabel!
+    @IBOutlet weak var sliderCount: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(SettingsTVC.preferredFontChange), name: UIContentSizeCategoryDidChangeNotification, object: nil)
+        
+        tableView.alwaysBounceVertical = false
+    }
+    
+    func preferredFontChange() {
+        
+        aboutDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        feedbackDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        securityDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        bestImageDisplay.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+        apiCount.font = UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)
+    }
+    
+    // Remove observer, called just as object is about to be deallocated
+    deinit {
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
+    }
+
+}
