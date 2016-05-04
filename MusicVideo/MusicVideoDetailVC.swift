9cd840aa4bb95343d724af9ccc972f70573baa63
photos <myjellymail@gmail.com>
2016-05-04 14:24:05 -0700

    Part 16 Completed

---
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  MusicVideoDetailVC.swift
+//  MusicVideo
+//
+//  Created by Forrest Collins on 5/4/16.
+//  Copyright © 2016 bananapps. All rights reserved.
+//
+
+import UIKit
+
+class MusicVideoDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
